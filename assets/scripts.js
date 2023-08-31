e89b1e064829f36edc19870802091bca9fac0827
Damien DRUAUX <druaux21@outlook.fr>
2023-08-31 16:34:36 +0200

    Replace scripts for vanilla JS and fix
    - Remove JQuery and maugallery
    - Add vanilla JS for gallery (filter)
    - Fix Rich Snippets
    - Replace h4 balise by p balise (outline)

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,31 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
+const navItem = document.querySelectorAll('.nav-link');
+
+navItem.forEach(element => {   
+    element.addEventListener('click', (e) => {
+        // Remove all active tag class on all elements
+        removeAllActiveTag();
+        
+        // Add the active and active tag class to the element
+        element.classList.add('active', 'active-tag');
+        //console.log(e.target.dataset.imagesToggle);
+
+        // Reload all image by the current dataset
+        reloadGallery(e.target.dataset.imagesToggle);
     });
 });
+
+function removeAllActiveTag() {
+    navItem.forEach(element => {
+        element.classList.remove('active', 'active-tag');
+    });
+}
+
+function reloadGallery(activeTag) {
+    document.querySelectorAll('.item-column').forEach(item => {
+        item.style.display = 'none';
+        
+        if(activeTag === "all" || activeTag === item.children[0].dataset.galleryTag) {
+            item.style.display = 'block';
+        }
+    });
+}
